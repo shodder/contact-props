b06f0a5c85079ae2dace057ba61d8b7a47216cc3
Simon Hodder <2083432+shodder@users.noreply.github.com>
2025-08-31 12:09:55 +0100

    Props learning with contact cards

--- App.jsx
+++ App.jsx
@@ -1,25 +1,51 @@
-/**
- * Challenge:
- * - Create a Contact component in another file
- * - Move one of the contact card articles below into that file
- * - import and render 4 instances of that contact card
- *     - Think ahead: what's the problem with doing it this way?
- */
+
 
 import Contact from "./Contact"
 
-function App() {
+export default function App() {
     return (
         <div className="contacts">
-            <Contact />
-            <Contact />
-            <Contact />
-            <Contact />
+            <Contact 
+                img="./images/mr-whiskerson.png"
+                name="Mr. Whiskerson"
+                phone="(212) 555-1234"
+                email="mr.whiskaz@catnap.meow"
+            />
+            <Contact
+                img="./images/fluffykins.png"
+                name="Fluffykins"
+                phone="(212) 555-2345"
+                email="fluff@me.com"
+            />
+            <Contact
+                img="./images/felix.png"
+                name="Felix"
+                phone="(212) 555-4567"
+                email="thecat@hotmail.com"
+            />
+            <Contact 
+                img="./images/pumpkin.png"
+                name="Pumpkin"
+                phone="(0800) CAT KING"
+                email="pumpkin@scrimba.com"
+            />
         </div>
     )
 }
 
-export default App
+// Hard coded version! Only works with one contact
+// function App() {
+//     return (
+//         <div className="contacts">
+//             <Contact />
+//             <Contact />
+//             <Contact />
+//             <Contact />
+//         </div>
+//     )
+// }
+
+// export default App
 
 /*
 function App() {
--- Contact.jsx
+++ Contact.jsx
@@ -1,29 +1,92 @@
 
 
-function Contact() {
+// Using props - basically pass in any/all attributes. The name props is what it is commonly called - but could be whatever!
+export default function Contact(props) {
+    // console.log(props)
     return (
         <article className="contact-card">
             <img 
-                src="./images/mr-whiskerson.png"
+                src={props.img}
                 alt="Photo of Mr. Whiskerson"
             />
-            <h3>Mr. Whiskerson</h3>
+            <h3>{props.name}</h3>
             <div className="info-group">
                 <img 
                     src="./images/phone-icon.png" 
                     alt="phone icon" 
                 />
-                <p>(212) 555-1234</p>
+                <p>{props.phone}</p>
             </div>
             <div className="info-group">
                 <img 
                     src="./images/mail-icon.png" 
                     alt="mail icon"
                 />
-                <p>mr.whiskaz@catnap.meow</p>
+                <p>{props.email}</p>
             </div>
         </article>
     )
+
 }
 
-export default Contact
+
+// Using props with destructuring
+//export default function Contact({img, name, phone, email}) {
+// export default function Contact({img: image, name, phone, email}) { // note the img: image is taking the img variable from the destuctured input and renaming it to image
+//     // console.log(props)
+//     return (
+//         <article className="contact-card">
+//             <img 
+//                 src={image}
+//                 alt="Photo of Mr. Whiskerson"
+//             />
+//             <h3>{name}</h3>
+//             <div className="info-group">
+//                 <img 
+//                     src="./images/phone-icon.png" 
+//                     alt="phone icon" 
+//                 />
+//                 <p>{phone}</p>
+//             </div>
+//             <div className="info-group">
+//                 <img 
+//                     src="./images/mail-icon.png" 
+//                     alt="mail icon"
+//                 />
+//                 <p>{email}</p>
+//             </div>
+//         </article>
+//     )
+
+// }
+
+
+
+
+// hard coded version
+// export default function Contact() {
+//     return (
+//         <article className="contact-card">
+//             <img 
+//                 src="./images/mr-whiskerson.png"
+//                 alt="Photo of Mr. Whiskerson"
+//             />
+//             <h3>Mr. Whiskerson</h3>
+//             <div className="info-group">
+//                 <img 
+//                     src="./images/phone-icon.png" 
+//                     alt="phone icon" 
+//                 />
+//                 <p>(212) 555-1234</p>
+//             </div>
+//             <div className="info-group">
+//                 <img 
+//                     src="./images/mail-icon.png" 
+//                     alt="mail icon"
+//                 />
+//                 <p>mr.whiskaz@catnap.meow</p>
+//             </div>
+//         </article>
+//     )
+// }
+
